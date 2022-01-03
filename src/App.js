f53a42891b7411f6c8f28a18d3e7eccad71940d8
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-01-03 19:03:39 -0300

    Set state to save the textarea value using useState hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import "App.css";
+import React, { useState } from "react";
+import "./css/App.css";
 
 function App() {
+	const { text, setText } = useState("");
+
+	function handleChange(event) {
+		const { value } = event.target;
+		setText(value);
+	}
+
 	return (
 		<div id="Game">
 			<header>
 				<h1>Speed Type Game</h1>
 			</header>
 			<main>
-				<textarea />
+				<textarea value={text} onChange={handleChange} />
 				<h4>Time remaining:</h4>
 				<button>Start</button>
 				<h1>Word count:</h1>
