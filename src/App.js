7384f03b1f4d1a7798b0c8e97d169c5b3cb1dda1
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-01-03 18:52:23 -0300

    Add the basic HTML structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import "App.css";
+
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+	return (
+		<div id="Game">
+			<header>
+				<h1>Speed Type Game</h1>
+			</header>
+			<main>
+				<textarea />
+				<h4>Time remaining:</h4>
+				<button>Start</button>
+				<h1>Word count:</h1>
+			</main>
+		</div>
+	);
 }
 
 export default App;
